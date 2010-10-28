1e18ba11bbeb5aabe24ea6293c27ba5c6b793103
Edi Modric <edi.modric@gmail.com>
2010-10-28 12:31:00 +0000

    Separated SQL schema and sample data

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -17,8 +17,3 @@ CREATE TABLE `eztags_attribute_link` (
   KEY `eztags_attr_link_kid_oaid` (`keyword_id`,`objectattribute_id`),
   KEY `eztags_attr_link_oaid` (`objectattribute_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `eztags` (`parent_id`, `keyword`, `modified`) VALUES(0, 'People', 1287705600);
-INSERT INTO `eztags` (`parent_id`, `keyword`, `modified`) VALUES(0, 'Places', 1287705600);
-INSERT INTO `eztags` (`parent_id`, `keyword`, `modified`) VALUES(0, 'Companies', 1287705600);
-INSERT INTO `eztags` (`parent_id`, `keyword`, `modified`) VALUES(0, 'Events', 1287705600);
