d838c9cdde2a3c9c0834b495a363eab2ec853f8f
Edi Modric <edi.modric@gmail.com>
2010-11-23 09:14:14 +0000

    Added Croatian translation

---
+++ translations/cro-HR/translation.ts
@@ -0,0 +1,152 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>extension/eztags/tags/edit</name>
+    <message>
+        <source>no parent</source>
+        <translation>bez nadređene oznake</translation>
+    </message>
+    <message>
+        <source>New tag</source>
+        <translation>Nova oznaka</translation>
+    </message>
+    <message>
+        <source>Tag name</source>
+        <translation>Ime oznake</translation>
+    </message>
+    <message>
+        <source>Parent tag</source>
+        <translation>Nadređena oznaka</translation>
+    </message>
+    <message>
+        <source>Select parent</source>
+        <translation>Odaberi nadređenu oznaku</translation>
+    </message>
+    <message>
+        <source>Add child tag</source>
+        <translation>Dodaj podređenu oznaku</translation>
+    </message>
+    <message>
+        <source>Edit tag</source>
+        <translation>Uredi oznaku</translation>
+    </message>
+    <message>
+        <source>Delete tag</source>
+        <translation>Izbriši oznaku</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spremi</translation>
+    </message>
+    <message>
+        <source>Discard</source>
+        <translation>Odustani</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to discard changes?</source>
+        <translation>Jeste li sigurni da želite odustati?</translation>
+    </message>
+    <message>
+        <source>Select parent element in tag tree</source>
+        <translation>Odaberite nadređenu oznaku iz stabla</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete the "%keyword" tag? All children tags will also be deleted and removed from existing objects.</source>
+        <translation>Jeste li sigurni da želite izbrisati oznaku "%keyword"? Sve podređene oznake će također biti izbrisane i uklonjene iz postojećih objekata.</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Da</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Ne</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/view</name>
+    <message>
+        <source>Tags Dashboard</source>
+        <translation>Kontrolna ploča oznaka</translation>
+    </message>
+    <message>
+        <source>Latest content</source>
+        <translation>Najnoviji sadržaj</translation>
+    </message>
+    <message>
+        <source>Related tags</source>
+        <translation>Povezane oznake</translation>
+    </message>
+    <message>
+        <source>Tag</source>
+        <translation>Oznaka</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/datatypes</name>
+    <message>
+        <source>Tags</source>
+        <translation>Oznake</translation>
+    </message>
+    <message>
+        <source>Loading</source>
+        <translation>Učitavanje</translation>
+    </message>
+    <message>
+        <source>Selected tags</source>
+        <translation>Odabrane oznake</translation>
+    </message>
+    <message>
+        <source>There are no selected tags</source>
+        <translation>Nema odabranih oznaka</translation>
+    </message>
+    <message>
+        <source>Suggested tags</source>
+        <translation>Predložene oznake</translation>
+    </message>
+    <message>
+        <source>There are no tags to suggest</source>
+        <translation>Nema predloženih oznaka</translation>
+    </message>
+    <message>
+        <source>Add new</source>
+        <translation>Dodaj kao novu</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/treemenu</name>
+    <message>
+        <source>Adding new tag - Select parent element in tag tree</source>
+        <translation>Dodavanje nove oznake - odaberite nadređenu oznaku</translation>
+    </message>
+    <message>
+        <source>Parent tag ID</source>
+        <translation>ID nadređene oznake</translation>
+    </message>
+    <message>
+        <source>Tag ID</source>
+        <translation>ID oznake</translation>
+    </message>
+    <message>
+        <source>Top Level Tags</source>
+        <translation>Vršne oznake</translation>
+    </message>
+    <message>
+        <source>Dynamic tree not allowed for this siteaccess</source>
+        <translation>Dinamičko stablo nije dopušteno u trenutnom siteaccessu</translation>
+    </message>
+    <message>
+        <source>Tag does not exist</source>
+        <translation>Oznaka ne postoji</translation>
+    </message>
+    <message>
+        <source>Internal error</source>
+        <translation>Interna greška</translation>
+    </message>
+    <message>
+        <source>Tags structure</source>
+        <translation>Struktura oznaka</translation>
+    </message>
+</context>
+</TS>
