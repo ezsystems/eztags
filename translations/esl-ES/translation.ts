724da9552b1e6c888705813050e516b7f6c8a452
crevillo <crevillo@gmail.com>
2011-03-05 19:35:11 +0100

    localization: added esl-ES translation.ts

---
+++ translations/esl-ES/translation.ts
@@ -0,0 +1,221 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>design/admin/parts/user/menu</name>
+    <message>
+        <source>Change the left menu width to small size.</source>
+        <translation>Cambiar el ancho del menú izquierdo al tamaño pequeño.</translation>
+    </message>
+    <message>
+        <source>Small</source>
+        <translation>Pequeño</translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation>Medio</translation>
+    </message>
+    <message>
+        <source>Change the left menu width to large size.</source>
+        <translation>Cambiar el ancho del menú izquierdo al tamaño grande.</translation>
+    </message>
+    <message>
+        <source>Large</source>
+        <translation>Grande</translation>
+    </message>
+    <message>
+        <source>Change the left menu width to medium size.</source>
+        <translation>Cambiar el ancho del menú izquierdo al tamaño mediano.</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/article/comments</name>
+    <message>
+        <source>%login_link_startLog in%login_link_end to comment.</source>
+        <translation>%login_link_startConéctate%login_link_end para comentar.</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/full/article</name>
+    <message>
+        <source>Comments</source>
+        <translation>Comentarios</translation>
+    </message>
+    <message>
+        <source>New comment</source>
+        <translation>Nuevo comentario</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to comment.</source>
+        <translation>%login_link_startConéctate%login_link_end o %create_link_startcrea una cuenta de usuario%create_link_end para comentar.</translation>
+    </message>
+    <message>
+        <source>Tip a friend</source>
+        <translation>Enviar a un amigo</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/datatypes</name>
+    <message>
+        <location filename="eztagstype.php" line="17"/>
+        <location filename="eztagstype.php" line="0"/>
+        <source>Tags</source>
+        <translation>Tags</translation>
+    </message>
+    <message>
+        <source>Selected tags</source>
+        <translation>Tags seleccionados</translation>
+    </message>
+    <message>
+        <source>Loading</source>
+        <translation>Cargando</translation>
+    </message>
+    <message>
+        <source>There are no selected tags</source>
+        <translation>No hay tags seleccionados</translation>
+    </message>
+    <message>
+        <source>Suggested tags</source>
+        <translation>Tags sugeridos</translation>
+    </message>
+    <message>
+        <source>There are no tags to suggest</source>
+        <translation>No hay tags sugeridos</translation>
+    </message>
+    <message>
+        <source>Add new</source>
+        <translation>Añadir nuevo</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/edit</name>
+    <message>
+        <location filename="eztagstemplatefunctions.php" line="78"/>
+        <source>no parent</source>
+        <translation>sin padre</translation>
+    </message>
+    <message>
+        <source>Add child tag</source>
+        <translation>Añadir tag hija</translation>
+    </message>
+    <message>
+        <source>Edit tag</source>
+        <translation>Editar tag</translation>
+    </message>
+    <message>
+        <source>Tag name</source>
+        <translation>Nombre del tag</translation>
+    </message>
+    <message>
+        <source>Parent tag</source>
+        <translation>Tag padre</translation>
+    </message>
+    <message>
+        <source>Select parent</source>
+        <translation>Escoger padre</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <source>Discard</source>
+        <translation>Descartar</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to discard changes?</source>
+        <translation>¿Seguro que quieres descartar los cambios?</translation>
+    </message>
+    <message>
+        <source>Select parent element in tag tree</source>
+        <translation>Seleccionar el elemento padre en el árbol de tags</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete the &quot;%keyword&quot; tag? All children tags will also be deleted and removed from existing objects.</source>
+        <translation>¿Seguro que quieres borrar el tag &quot;%keyword&quot;? Todos los tags hijos serán también eliminados de los objetos existentes.</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Sí</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <location filename="delete.php" line="57"/>
+        <source>Delete tag</source>
+        <translation>Eliminar tag</translation>
+    </message>
+    <message>
+        <location filename="add.php" line="87"/>
+        <source>New tag</source>
+        <translation>Nuevo tag</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/treemenu</name>
+    <message>
+        <source>Tags structure</source>
+        <translation>Estructura de tags</translation>
+    </message>
+    <message>
+        <source>Tag ID</source>
+        <translation>Id del tag</translation>
+    </message>
+    <message>
+        <source>Parent tag ID</source>
+        <translation>Id del tag padre</translation>
+    </message>
+    <message>
+        <source>Dynamic tree not allowed for this siteaccess</source>
+        <translation>El árbol dinámico no está permitido en este siteaccess</translation>
+    </message>
+    <message>
+        <source>Tag does not exist</source>
+        <translation>El tag no existe</translation>
+    </message>
+    <message>
+        <source>Internal error</source>
+        <translation>Error interno</translation>
+    </message>
+    <message>
+        <source>Top Level Tags</source>
+        <translation>Tags del nivel superior</translation>
+    </message>
+    <message>
+        <source>Adding new tag - Select parent element in tag tree</source>
+        <translation>Añadiendo nuevo tag - Seleccionar elemento padre en el árbol de tags</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/view</name>
+    <message>
+        <source>Tag</source>
+        <translation>Tag</translation>
+    </message>
+    <message>
+        <source>Latest content</source>
+        <translation>Último contenido</translation>
+    </message>
+    <message>
+        <source>Related tags</source>
+        <translation>Tags relacionados</translation>
+    </message>
+    <message>
+        <location filename="dashboard.php" line="12"/>
+        <source>Tags Dashboard</source>
+        <translation>Escritorio de tags</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/classes/datatypes</name>
+    <message>
+        <location filename="eztagstype.php" line="70"/>
+        <location filename="eztagstype.php" line="78"/>
+        <location filename="eztagstype.php" line="85"/>
+        <source>Input required.</source>
+        <translation>Entrada obligatoria.</translation>
+    </message>
+</context>
+</TS>
