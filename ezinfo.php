7e918fde6bbcea4291f0f07582f5035664213649
Edi Modric <edi.modric@gmail.com>
2011-03-08 10:14:18 +0000

    Bump version to 1.0 beta

--- ezinfo.php
+++ ezinfo.php
@@ -2,7 +2,7 @@
 //
 // ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 // SOFTWARE NAME: eZ Tags
-// SOFTWARE RELEASE: 0.1.0
+// SOFTWARE RELEASE: 1.0 beta
 // COPYRIGHT NOTICE: Copyright (C) 1999-2010 eZ Systems AS
 // SOFTWARE LICENSE: GNU General Public License v2.0
 // NOTICE: >
@@ -26,10 +26,10 @@ class eztagsInfo
 {
     static function info()
     {
-        $eZCopyrightString = 'Copyright (C) 2010 Netgen d.o.o., 1999-2010 eZ Systems AS';
+        $eZCopyrightString = 'Copyright (C) 2011 Netgen d.o.o., 1999-2011 eZ Systems AS';
 
-        return array( 'Name'      => '<a href="http://ez.no">eZ Tags</a> extension',
-                      'Version'   => '1.0 alpha',
+        return array( 'Name'      => '<a href="http://projects.ez.no/eztags">eZ Tags</a> extension',
+                      'Version'   => '1.0 beta',
                       'Copyright' => $eZCopyrightString,
                       'License'   => 'GNU General Public License v2.0'
                     );
