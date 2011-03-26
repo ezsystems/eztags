373fae28eb31e740517377268440a4938516c69f
Edi Modric <edi.modric@gmail.com>
2011-03-26 13:31:23 +0000

    Added French translation, thanks to Frederic DAVID (https://github.com/frefred34)

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,399 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+<name>design/standard/class/datatype</name>
+    <message>
+        <source>Limit by tags subtree</source>
+        <translation>Limiter à une sous arborescence de tag</translation>
+    </message>
+    <message>
+        <source>Show dropdown instead of autocomplete</source>
+        <translation>Afficher une liste déroulante au lieu de l'autocomplétion</translation>
+    </message>
+    <message>
+        <source>Select subtree</source>
+        <translation>Sélectionner une sous-arborescence</translation>
+    </message>
+    <message>
+        <source>No limit</source>
+        <translation>Aucune limite</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Oui</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Non</translation>
+    </message>
+</context>
+<context>
+<name>extension/eztags/errors</name>
+    <message>
+        <source>Tag cannot be modified because it is being used as subtree limitation in one or more class attributes.</source>
+        <translation>Le tag ne peut as être modifié car il est utilisé comme limitation d'arborescence par un ou plusieurs attributs de classes.</translation>
+    </message>
+    <message>
+        <source>Selected target tag is invalid.</source>
+        <translation>Le tag parent sélectionné n'est pas valide.</translation>
+    </message>
+    <message>
+        <source>Name cannot be empty.</source>
+        <translation>Le nom ne peut pas être vide.</translation>
+    </message>
+    <message>
+        <source>Go back</source>
+        <translation>Retour en arrière</translation>
+    </message>
+</context>
+<context>
+<name>extension/eztags/warnings</name>
+    <message>
+        <source>TAKE CARE: Tag is inside class attribute subtree limit(s). If moved outside those limits, it could lead to inconsistency as objects could end up with tags that they are not supposed to have.</source>
+        <translation type="unfinished">ATTENTTION: Le tag est dans les limites d'arborescence de l'attribut de classe. Si il est déplacé en dehors de ces limits, cela peut provoquer </translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/dashboard</name>
+    <message>
+        <source>Latest tags</source>
+        <translation>Derniers tags</translation>
+    </message>
+    <message>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <source>Tag name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <source>Parent tag name</source>
+        <translation>Tag parent</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Modifié</translation>
+    </message>
+    <message>
+        <source>No parent</source>
+        <translation>Aucun parent</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/edit</name>
+    <message>
+        <source>no parent</source>
+        <translation>aucun parent</translation>
+    </message>
+    <message>
+        <source>New tag</source>
+        <translation>Nouveau tag</translation>
+    </message>
+    <message>
+        <source>New synonym tag</source>
+        <translation>Nouveau synonyme</translation>
+    </message>
+    <message>
+        <source>Main tag ID</source>
+        <translation>ID du tag principal</translation>
+    </message>
+    <message>
+        <source>Main tag name</source>
+        <translation>Nom du tag principal</translation>
+    </message>
+    <message>
+        <source>Tag name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <source>Synonym name</source>
+        <translation>Nom du synonyme</translation>
+    </message>
+    <message>
+        <source>Parent tag</source>
+        <translation>Tag parent</translation>
+    </message>
+    <message>
+        <source>Select parent</source>
+        <translation>Parent sélectionné</translation>
+    </message>
+    <message>
+        <source>Add child tag</source>
+        <translation>Créer un tag en enfant</translation>
+    </message>
+    <message>
+        <source>Add synonym</source>
+        <translation>Ajouter un synonyme</translation>
+    </message>
+    <message>
+        <source>Convert to synonym</source>
+        <translation>Convertir en synonyme</translation>
+    </message>
+    <message>
+        <source>Edit tag</source>
+        <translation>Modifier le tag</translation>
+    </message>
+    <message>
+        <source>Edit synonym</source>
+        <translation>Modifier le synonyme</translation>
+    </message>
+    <message>
+        <source>Delete tag</source>
+        <translation>Supprimer le tag</translation>
+    </message>
+    <message>
+        <source>Merge tag</source>
+        <translation>Fusionner le tag</translation>
+    </message>
+    <message>
+        <source>Delete synonym</source>
+        <translation>Supprimer le synonyme</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Sauvegarder</translation>
+    </message>
+    <message>
+        <source>Discard</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to discard changes?</source>
+        <translation>Etes vous sur de vouloir annuler les changements ?</translation>
+    </message>
+    <message>
+        <source>Main tag</source>
+        <translation>Tag principal</translation>
+    </message>
+    <message>
+        <source>Select main tag</source>
+        <translation>Sélectionner le tag principal</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete the "%keyword" tag? All children tags and synonyms will also be deleted and removed from existing objects.</source>
+        <translation>Etes vous sur de vouloir supprimer le tag "%keyword" ? Tous les tags et synonymes en enfant seront aussi supprimés et enlevé des objets existants.</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete the "%keyword" synonym?</source>
+        <translation>Etes vous sur de vouloir supprimer le synonyme "%keyword" ?</translation>
+    </message>
+    <message>
+        <source>Transfer all related objects to the main tag</source>
+        <translation>Transférer tous les objets en relation au tag principal</translation>
+    </message>
+    <message>
+        <source>Merging this tag with another tag will delete the tag and it's synonyms and transfer all related objects to the main tag. Also, all children tags will become main tag children.</source>
+        <translation>Fusionner ce tag avec un autre tag détruira le tag, ses synonymes et transfera tous les objets en relation au tag principal. Tous les tags en enfant passeront en enfant du tag principal.</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Oui</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Non</translation>
+    </message>
+    <message>
+        <source>The tag you're about to delete has</source>
+        <translation>Le tag que vous allez supprimer est</translation>
+    </message>
+    <message>
+        <source>The tag you're about to merge has</source>
+        <translation>Le tag que vous allez fusionner est</translation>
+    </message>
+    <message>
+        <source>number of first level children tags</source>
+        <translation>Nombre de tags en enfant de premier niveau</translation>
+    </message>
+    <message>
+        <source>number of objects related to first level children tags</source>
+        <translation>Nombre d'objets en relaion aux tags de premier niveau</translation>
+    </message>
+    <message>
+        <source>number of synonyms</source>
+        <translation>Nombre de synonymes</translation>
+    </message>
+    <message>
+        <source>number of objects related to synonyms</source>
+        <translation>Nombre d'objets en relation au synonyme</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/view</name>
+    <message>
+        <source>Tags Dashboard</source>
+        <translation>Tableau de bord</translation>
+    </message>
+    <message>
+        <source>Latest content</source>
+        <translation>Derniers contenus</translation>
+    </message>
+    <message>
+        <source>No content</source>
+        <translation>Aucun contenu</translation>
+    </message>
+    <message>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Modifié</translation>
+    </message>
+    <message>
+        <source>Related tags</source>
+        <translation>Tags en relation</translation>
+    </message>
+    <message>
+        <source>Tag</source>
+        <translation>Tag</translation>
+    </message>
+    <message>
+        <source>Synonym</source>
+        <translation>Synonyme</translation>
+    </message>
+    <message>
+        <source>Synonyms</source>
+        <translation>Synonymes</translation>
+    </message>
+    <message>
+        <source>No synonyms</source>
+        <translation>Aucun synonyme</translation>
+    </message>
+    <message>
+        <source>Main tag</source>
+        <translation>Tag principal</translation>
+    </message>
+    <message>
+        <source>Subtree limitations</source>
+        <translation>Limitations d'arborescence</translation>
+    </message>
+    <message>
+        <source>Class ID</source>
+        <translation>ID de la classe</translation>
+    </message>
+    <message>
+        <source>Class name</source>
+        <translation>Nom de la classe</translation>
+    </message>
+    <message>
+        <source>Attribute identifier</source>
+        <translation>Identifiant de l'attribute</translation>
+    </message>
+    <message>
+        <source>No subtree limitations</source>
+        <translation>Aucune limitation d'arborescence</translation>
+    </message>
+</context>
+<context>
+<name>extension/eztags/tags/search</name>
+    <message>
+        <source>Tags search</source>
+        <translation>Chercher parmis les tags</translation>
+    </message>
+    <message>
+        <source>Search tags</source>
+        <translation>Chercher dans les tags</translation>
+    </message>
+    <message>
+        <source>Include synonyms in search</source>
+        <translation>Inclure les synonymes dans la recherche</translation>
+    </message>
+    <message>
+        <source>Empty search not allowed. Please enter your search query above.</source>
+        <translation>Les recherches vides ne sont pas permises. Merci d'entrer votre recherche dans le champ ci-dessus.</translation>
+    </message>
+    <message>
+        <source>No tags were found while searching for "%1".</source>
+        <translation>Aucun tag trouvé pour votre recherche "%1".</translation>
+    </message>
+    <message>
+        <source>Search for "%1" returned %2 matches.</source>
+        <translation>Votre recherche pour "%1" a retourné %2 résultats.</translation>
+    </message>
+    <message>
+        <source>Tag name</source>
+        <translation>Nom du tag</translation>
+    </message>
+    <message>
+        <source>Parent tag</source>
+        <translation>Parent</translation>
+    </message>
+    <message>
+        <source>Main tag name</source>
+        <translation>Nom du tag principal</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/datatypes</name>
+    <message>
+        <source>Tags</source>
+        <translation>Tags</translation>
+    </message>
+    <message>
+        <source>Loading</source>
+        <translation>Chargement</translation>
+    </message>
+    <message>
+        <source>Selected tags</source>
+        <translation>Tags sélectionnés</translation>
+    </message>
+    <message>
+        <source>There are no selected tags</source>
+        <translation>Il y a aucun tag sélectionné</translation>
+    </message>
+    <message>
+        <source>Suggested tags</source>
+        <translation>Tags suggérés</translation>
+    </message>
+    <message>
+        <source>There are no tags to suggest</source>
+        <translation>Il n'y a ancun tag suggéré</translation>
+    </message>
+    <message>
+        <source>Add new</source>
+        <translation>Ajouter un nouveau tag</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/treemenu</name>
+    <message>
+        <source>Select tag</source>
+        <translation>Tag sélectionné</translation>
+    </message>
+    <message>
+        <source>Parent tag ID</source>
+        <translation>ID du tag parent</translation>
+    </message>
+    <message>
+        <source>Tag ID</source>
+        <translation>ID du tag</translation>
+    </message>
+    <message>
+        <source>Top Level Tags</source>
+        <translation>Racine des tags</translation>
+    </message>
+    <message>
+        <source>Dynamic tree not allowed for this siteaccess</source>
+        <translation>Les arborescences dynamiques ne sont pas autorisées pour ce siteaccess</translation>
+    </message>
+    <message>
+        <source>Tag does not exist</source>
+        <translation>Ce tag n'existe pas</translation>
+    </message>
+    <message>
+        <source>Internal error</source>
+        <translation>Erreur interne</translation>
+    </message>
+    <message>
+        <source>Tags structure</source>
+        <translation>Structure des tags</translation>
+    </message>
+</context>
+</TS>
