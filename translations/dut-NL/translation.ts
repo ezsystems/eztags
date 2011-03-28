4ba294bdaebd96e17d179cb785eb988b4f018e8b
svda <sander@myxt.nl>
2011-03-28 22:07:50 +0200

    Added dutch translation.

---
+++ translations/dut-NL/translation.ts
@@ -0,0 +1,429 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl_NL">
+<context>
+    <name>design/admin/parts/user/menu</name>
+    <message>
+        <source>Change the left menu width to small size.</source>
+        <translation>Het linker menu smaller maken.</translation>
+    </message>
+    <message>
+        <source>Small</source>
+        <translation>Smal</translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation>Middel</translation>
+    </message>
+    <message>
+        <source>Change the left menu width to large size.</source>
+        <translation>Het linker menu breder maken.</translation>
+    </message>
+    <message>
+        <source>Large</source>
+        <translation>Breed</translation>
+    </message>
+    <message>
+        <source>Change the left menu width to medium size.</source>
+        <translation>Het linker menu standaard breedte maken.</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/article/comments</name>
+    <message>
+        <source>%login_link_startLog in%login_link_end to comment.</source>
+        <translation>%login_link_startLog in%login_link_end om te reageren.</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/full/article</name>
+    <message>
+        <source>Comments</source>
+        <translation>Reacties</translation>
+    </message>
+    <message>
+        <source>New comment</source>
+        <translation>Nieuwe reactie</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to comment.</source>
+        <translation>%login_link_startLog in%login_link_of %create_link_startmaak een gebruikersaccount aan%create_link_end om te reageren.</translation>
+    </message>
+    <message>
+        <source>Tip a friend</source>
+        <translation>Doorsturen</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/class/datatype</name>
+    <message>
+        <source>Limit by tags subtree</source>
+        <translation>Beperken tot een deel van de label boomstructuur</translation>
+    </message>
+    <message>
+        <source>Select subtree</source>
+        <translation>Onderliggende boomstructuur selecteren</translation>
+    </message>
+    <message>
+        <source>Show dropdown instead of autocomplete</source>
+        <translation>Dropdown in plaats van autocomplete tonen</translation>
+    </message>
+    <message>
+        <source>No limit</source>
+        <translation>Geen limiet</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Nee</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/datatypes</name>
+    <message>
+        <source>Selected tags</source>
+        <translation>Labels selecteren</translation>
+    </message>
+    <message>
+        <source>Loading</source>
+        <translation>Bezig met laden</translation>
+    </message>
+    <message>
+        <source>There are no selected tags</source>
+        <translation>Er zijn geen labels geselecteerd</translation>
+    </message>
+    <message>
+        <source>Suggested tags</source>
+        <translation>Voorgestelde labels</translation>
+    </message>
+    <message>
+        <source>There are no tags to suggest</source>
+        <translation>Er zijn geen labels ter suggestie</translation>
+    </message>
+    <message>
+        <source>Add new</source>
+        <translation>Nieuwe toevoegen</translation>
+    </message>
+    <message>
+        <source>Tags</source>
+        <translation>Labels</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/errors</name>
+    <message>
+        <source>Go back</source>
+        <translation>Terug</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/dashboard</name>
+    <message>
+        <source>Latest tags</source>
+        <translation>Nieuwste labels</translation>
+    </message>
+    <message>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <source>Tag name</source>
+        <translation>Labelnaam</translation>
+    </message>
+    <message>
+        <source>Parent tag name</source>
+        <translation>Naam van bovenliggende label</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Aangepast</translation>
+    </message>
+    <message>
+        <source>No parent</source>
+        <translation>Geen bovenliggende</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/edit</name>
+    <message>
+        <source>Edit synonym</source>
+        <translation>Synoniem bewerken</translation>
+    </message>
+    <message>
+        <source>Delete synonym</source>
+        <translation>Synoniem verwijderen</translation>
+    </message>
+    <message>
+        <source>Add child tag</source>
+        <translation>Onderliggend label toevoegen</translation>
+    </message>
+    <message>
+        <source>Edit tag</source>
+        <translation>Label bewerken</translation>
+    </message>
+    <message>
+        <source>Delete tag</source>
+        <translation>Label verwijderen</translation>
+    </message>
+    <message>
+        <source>Merge tag</source>
+        <translation>Label samenvoegen</translation>
+    </message>
+    <message>
+        <source>Add synonym</source>
+        <translation>Synoniem toevoegen</translation>
+    </message>
+    <message>
+        <source>Convert to synonym</source>
+        <translation>Veranderen in synoniem</translation>
+    </message>
+    <message>
+        <source>New tag</source>
+        <translation>Nieuw label</translation>
+    </message>
+    <message>
+        <source>Tag name</source>
+        <translation>Naam van het label</translation>
+    </message>
+    <message>
+        <source>Parent tag</source>
+        <translation>Bovenliggend label</translation>
+    </message>
+    <message>
+        <source>Select parent</source>
+        <translation>Bovenliggende selecteren</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Opslaan</translation>
+    </message>
+    <message>
+        <source>Discard</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to discard changes?</source>
+        <translation>Weet u zeker dat u de wijzigingen wilt annuleren?</translation>
+    </message>
+    <message>
+        <source>New synonym tag</source>
+        <translation>Nieuw synoniem label</translation>
+    </message>
+    <message>
+        <source>Main tag ID</source>
+        <translation>Hoofdlabel ID</translation>
+    </message>
+    <message>
+        <source>Main tag name</source>
+        <translation>Hoofdlabel naam</translation>
+    </message>
+    <message>
+        <source>Synonym name</source>
+        <translation>Naam van het synoniem</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete the &quot;%keyword&quot; tag? All children tags and synonyms will also be deleted and removed from existing objects.</source>
+        <translation>Weet u zeker dat u de tag &quot;%keyword&quot; wilt verwijderen? Alle onderliggende labels en synoniemen worden ook verwijderd uit bestaande objecten.</translation>
+    </message>
+    <message>
+        <source>The tag you&apos;re about to delete has</source>
+        <translation>Het label dat u wilt verwijderen heeft</translation>
+    </message>
+    <message>
+        <source>number of first level children tags</source>
+        <translation>aantal direct onderliggende labels</translation>
+    </message>
+    <message>
+        <source>number of objects related to first level children tags</source>
+        <translation>aantal objecten gerelateerd aan direct onderliggende labels</translation>
+    </message>
+    <message>
+        <source>number of synonyms</source>
+        <translation>aantal synoniemen</translation>
+    </message>
+    <message>
+        <source>number of objects related to synonyms</source>
+        <translation>Aantal objecten gerelateerd aan synoniemen</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Nee</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete the &quot;%keyword&quot; synonym?</source>
+        <translation>Weet u zeker dat u het synoniem &quot;%keyword&quot; wilt verwijderen?</translation>
+    </message>
+    <message>
+        <source>Transfer all related objects to the main tag</source>
+        <translation>Alle gerelateerde objecten naar het hoofdlabel verplaatsen</translation>
+    </message>
+    <message>
+        <source>Main tag</source>
+        <translation>Hoofdlabel</translation>
+    </message>
+    <message>
+        <source>Select main tag</source>
+        <translation>Hoofdlabel selecteren</translation>
+    </message>
+    <message>
+        <source>Merging this tag with another tag will delete the tag and it&apos;s synonyms and transfer all related objects to the main tag. Also, all children tags will become main tag children.</source>
+        <translation>Bij het samenvoegen van dit label met een andere tag worden het label en synoniemen verwijderd en  worden alle gerelateerde objecten aan het hoofdlabel gekoppeld. Ook worden onderliggende labels veranderd in onderliggende labels van het hoofdlabel.</translation>
+    </message>
+    <message>
+        <source>The tag you&apos;re about to merge has</source>
+        <translation>Het label dat u wilt samenvoegen heeft</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/search</name>
+    <message>
+        <source>Tags search</source>
+        <translation>Labels zoeken</translation>
+    </message>
+    <message>
+        <source>Search tags</source>
+        <translation>Labels zoeken</translation>
+    </message>
+    <message>
+        <source>Include synonyms in search</source>
+        <translation>Synoniemen ook doorzoeken</translation>
+    </message>
+    <message>
+        <source>Empty search not allowed. Please enter your search query above.</source>
+        <translation>Zoekterm verplicht. Voer hierboven een zoekterm in.</translation>
+    </message>
+    <message>
+        <source>No tags were found while searching for &quot;%1&quot;.</source>
+        <translation>Geen labels gevonden bij het zoeken naar &quot;%1&quot;.</translation>
+    </message>
+    <message>
+        <source>Search for &quot;%1&quot; returned %2 matches.</source>
+        <translation>Bij het zoeken naar &quot;%1&quot; zijn  %2 resultaten gevonden.</translation>
+    </message>
+    <message>
+        <source>Tag name</source>
+        <translation>Naam label</translation>
+    </message>
+    <message>
+        <source>Parent tag</source>
+        <translation>Bovenliggend label</translation>
+    </message>
+    <message>
+        <source>Main tag name</source>
+        <translation>Naam hoofdlabel</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/treemenu</name>
+    <message>
+        <source>Tags structure</source>
+        <translation>Labelstructuur</translation>
+    </message>
+    <message>
+        <source>Tag ID</source>
+        <translation>Label ID</translation>
+    </message>
+    <message>
+        <source>Parent tag ID</source>
+        <translation>Bovenliggend label ID</translation>
+    </message>
+    <message>
+        <source>Dynamic tree not allowed for this siteaccess</source>
+        <translation>Dynamische boomstructuur niet toegestaan in deze siteaccess</translation>
+    </message>
+    <message>
+        <source>Tag does not exist</source>
+        <translation>Label bestaat niet</translation>
+    </message>
+    <message>
+        <source>Internal error</source>
+        <translation>interne foutmelding</translation>
+    </message>
+    <message>
+        <source>Top Level Tags</source>
+        <translation>Labels op hoogste niveau</translation>
+    </message>
+    <message>
+        <source>Select tag</source>
+        <translation>Label selecteren</translation>
+    </message>
+</context>
+<context>
+    <name>extension/eztags/tags/view</name>
+    <message>
+        <source>Tags Dashboard</source>
+        <translation>Labels Dashboard</translation>
+    </message>
+    <message>
+        <source>Latest content</source>
+        <translation>Laatste inhoud</translation>
+    </message>
+    <message>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Aangepast</translation>
+    </message>
+    <message>
+        <source>No content</source>
+        <translation>Geen inhoud</translation>
+    </message>
+    <message>
+        <source>Subtree limitations</source>
+        <translation>Beperkingen in boomstructuur</translation>
+    </message>
+    <message>
+        <source>Class ID</source>
+        <translation>Type ID</translation>
+    </message>
+    <message>
+        <source>Class name</source>
+        <translation>Type naam</translation>
+    </message>
+    <message>
+        <source>Attribute identifier</source>
+        <translation>Attribuut identifier</translation>
+    </message>
+    <message>
+        <source>No subtree limitations</source>
+        <translation>Geen beperkingen in boomstructuur</translation>
+    </message>
+    <message>
+        <source>Synonyms</source>
+        <translation>Synoniemen</translation>
+    </message>
+    <message>
+        <source>No synonyms</source>
+        <translation>Geen synoniemen</translation>
+    </message>
+    <message>
+        <source>Tag</source>
+        <translation>Label</translation>
+    </message>
+    <message>
+        <source>Synonym</source>
+        <translation>Synoniem</translation>
+    </message>
+    <message>
+        <source>Main tag</source>
+        <translation>Hoofdlabel</translation>
+    </message>
+    <message>
+        <source>Related tags</source>
+        <translation>Gerelateerde labels</translation>
+    </message>
+</context>
+</TS>
