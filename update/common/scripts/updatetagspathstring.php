a620666a8d0fc6792e432cf026773b2379ad333d
Edi Modric <edi.modric@gmail.com>
2011-03-28 11:20:56 +0000

    Added upgrade instructions. IMPORTANT: read doc/UPGRADE due to breaking change introduced with commit 373fae2

---
+++ update/common/scripts/updatetagspathstring.php
@@ -0,0 +1,45 @@
+#!/usr/bin/env php
+<?php
+
+require 'autoload.php';
+
+$cli = eZCLI::instance();
+
+$script = eZScript::instance( array( 'description' => ( "\nUpdates path string of all the tags.\n" ),
+                                     'use-session' => false,
+                                     'use-modules' => false,
+                                     'use-extensions' => true ) );
+
+$script->startup();
+
+$options = $script->getOptions( '', '', array() );
+$script->initialize();
+
+$limit = 20;
+$offset = 0;
+
+$db = eZDB::instance();
+
+$script->setIterationData( '.', '~' );
+
+while ( $firstLevelTags = eZPersistentObject::fetchObjectList( eZTagsObject::definition(), null, array( 'parent_id' => 0, 'main_tag_id' => 0 ), null, array( 'offset' => $offset, 'limit' => $limit ) ) )
+{
+    foreach ( $firstLevelTags as $tag )
+    {
+        $tagID = $tag->ID;
+
+        $db->begin();
+
+        $tag->updatePathString(false);
+
+        $db->commit();
+
+        $script->iterate( $cli, true, 'Updated path string of tag ID = $tagID and all its children.' );
+    }
+
+    $offset += $limit;
+}
+
+$script->shutdown();
+
+?>
---
+++ update/database/mysql/1.0/unstable/eztags-dbupdate-1.0beta-to-1.0.sql
@@ -0,0 +1,22 @@
+ALTER TABLE `eztags`
+ADD COLUMN `main_tag_id` int(11) NOT NULL default '0' AFTER `parent_id`;
+
+ALTER TABLE `eztags`
+ADD COLUMN `path_string` varchar(255) NOT NULL default '' AFTER `keyword`;
+
+-- START: Versioning support
+
+ALTER TABLE `eztags_attribute_link`
+ADD COLUMN `objectattribute_version` int(11) NOT NULL default '0' AFTER `objectattribute_id`;
+
+UPDATE eztags_attribute_link l
+INNER JOIN ezcontentobject o ON l.object_id = o.id
+SET l.objectattribute_version = o.current_version;
+
+DROP INDEX `eztags_attr_link_kid_oaid` ON `eztags_attribute_link`;
+DROP INDEX `eztags_attr_link_oaid` ON `eztags_attribute_link`;
+
+CREATE INDEX `eztags_attr_link_kid_oaid_oav` ON `eztags_attribute_link` (`keyword_id`,`objectattribute_id`,`objectattribute_version`);
+CREATE INDEX `eztags_attr_link_oaid_oav` ON `eztags_attribute_link` (`objectattribute_id`,`objectattribute_version`);
+
+-- END: Versioning support
