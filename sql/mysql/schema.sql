6d6a3d3b3774b50640f724c127a31e59dacf8302
Edi Modric <edi.modric@gmail.com>
2011-09-16 10:16:10 +0000

    Update schema to support multilanguage tags

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -6,6 +6,8 @@ CREATE TABLE `eztags` (
   `depth` int(11) NOT NULL default '1',
   `path_string` varchar(255) NOT NULL default '',
   `modified` int(11) NOT NULL default '0',
+  `main_language_id` int(11) NOT NULL default '0',
+  `language_mask` int(11) NOT NULL default '0',
   PRIMARY KEY ( `id` ),
   KEY `eztags_keyword` ( `keyword` ),
   KEY `eztags_keyword_id` ( `keyword`, `id` )
@@ -22,4 +24,12 @@ CREATE TABLE `eztags_attribute_link` (
   KEY `eztags_attr_link_kid_oaid_oav` ( `keyword_id`, `objectattribute_id`, `objectattribute_version` ),
   KEY `eztags_attr_link_kid_oid` ( `keyword_id`, `object_id` ),
   KEY `eztags_attr_link_oaid_oav` ( `objectattribute_id`, `objectattribute_version` )
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `eztags_keyword` (
+  `keyword_id` int(11) NOT NULL,
+  `language_id` int(11) NOT NULL,
+  `keyword` varchar(255) NOT NULL default '',
+  `locale` varchar(255) NOT NULL default '',
+  PRIMARY KEY ( `keyword_id`, `language_id` )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
