bdf70c1fa982f602c55b4b627ba1cd00f3570c0a
Edi Modric <edi.modric@gmail.com>
2011-09-16 10:22:05 +0000

    Remove sample data and references to it

--- sql/mysql/samples.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `eztags` ( `parent_id`, `main_tag_id`, `keyword`, `depth`, `path_string`, `modified` ) VALUES( 0, 0, 'People', 1, '/1/', 1287705600 );
-INSERT INTO `eztags` ( `parent_id`, `main_tag_id`, `keyword`, `depth`, `path_string`, `modified` ) VALUES( 0, 0, 'Places', 1, '/2/', 1287705600 );
-INSERT INTO `eztags` ( `parent_id`, `main_tag_id`, `keyword`, `depth`, `path_string`, `modified` ) VALUES( 0, 0, 'Companies', 1, '/3/', 1287705600 );
-INSERT INTO `eztags` ( `parent_id`, `main_tag_id`, `keyword`, `depth`, `path_string`, `modified` ) VALUES( 0, 0, 'Events', 1, '/4/', 1287705600 );
