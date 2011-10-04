d186704cef5e68f1cb7507f5d0437332b2c4f5d2
Edi Modric <edi.modric@gmail.com>
2011-10-04 08:22:12 +0000

    Strip whitespaces

--- modules/tags/translation.php
+++ modules/tags/translation.php
@@ -17,36 +17,36 @@ if ( !$tag instanceof eZTagsObject )
 
 if ( $http->hasPostVariable( 'RemoveTranslationButton' ) )
 {
-	if ( $http->hasPostVariable( 'LanguageID' ) && is_array( $http->postVariable( 'LanguageID' ) ) )
-	{
-		$db->begin();
-		foreach ( $http->postVariable( 'LanguageID' ) as $languageID )
-		{
-			$translation = $tag->translationByLanguageID( (int) $languageID );
-			if ( $translation instanceof eZTagsKeyword && $translation->attribute( 'language_id' ) != $tag->attribute( 'main_language_id' ) )
-			{
-				$translation->remove();
-			}
-		}
-		$db->commit();
-	}
+    if ( $http->hasPostVariable( 'LanguageID' ) && is_array( $http->postVariable( 'LanguageID' ) ) )
+    {
+        $db->begin();
+        foreach ( $http->postVariable( 'LanguageID' ) as $languageID )
+        {
+            $translation = $tag->translationByLanguageID( (int) $languageID );
+            if ( $translation instanceof eZTagsKeyword && $translation->attribute( 'language_id' ) != $tag->attribute( 'main_language_id' ) )
+            {
+                $translation->remove();
+            }
+        }
+        $db->commit();
+    }
 }
 else if ( $http->hasPostVariable( 'UpdateMainLanguageButton' ) )
 {
-	if ( $http->hasPostVariable( 'MainLanguageID' ) )
-	{
-		$db->begin();
-		$tag->updateMainTranslation( $http->postVariable( 'MainLanguageID' ), true );
-		$tag->updateModified();
-		$db->commit();
-	}
+    if ( $http->hasPostVariable( 'MainLanguageID' ) )
+    {
+        $db->begin();
+        $tag->updateMainTranslation( $http->postVariable( 'MainLanguageID' ), true );
+        $tag->updateModified();
+        $db->commit();
+    }
 }
 else if ( $http->hasPostVariable( 'UpdateAlwaysAvailableButton' ) )
 {
-	$db->begin();
-	$alwaysAvailable = $http->hasPostVariable( 'AlwaysAvailable' );
-	$tag->setAlwaysAvailable( $alwaysAvailable, true );
-	$db->commit();
+    $db->begin();
+    $alwaysAvailable = $http->hasPostVariable( 'AlwaysAvailable' );
+    $tag->setAlwaysAvailable( $alwaysAvailable, true );
+    $db->commit();
 }
 
 return $Module->redirectToView( 'id', array( $tag->attribute( 'id' ) ) );
