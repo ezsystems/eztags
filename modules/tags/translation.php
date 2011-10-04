d595692ac7f7f5123fbfa66d5f92b26a01b14ddb
Edi Modric <edi.modric@gmail.com>
2011-10-04 08:01:49 +0000

    Implement tags/translation

--- modules/tags/translation.php
+++ modules/tags/translation.php
@@ -1,8 +1,9 @@
 <?php
 
+$db = eZDB::instance();
 $http = eZHTTPTool::instance();
-$tagID = $http->hasPostVariable( 'TagID' ) ? (int) $http->postVariable( 'TagID' ) : 0;
 
+$tagID = $http->hasPostVariable( 'TagID' ) ? (int) $http->postVariable( 'TagID' ) : 0;
 if ( $tagID <= 0 )
 {
     return $Module->handleError( eZError::KERNEL_NOT_FOUND, 'kernel' );
@@ -16,12 +17,36 @@ if ( !$tag instanceof eZTagsObject )
 
 if ( $http->hasPostVariable( 'RemoveTranslationButton' ) )
 {
+	if ( $http->hasPostVariable( 'LanguageID' ) && is_array( $http->postVariable( 'LanguageID' ) ) )
+	{
+		$db->begin();
+		foreach ( $http->postVariable( 'LanguageID' ) as $languageID )
+		{
+			$translation = $tag->translationByLanguageID( (int) $languageID );
+			if ( $translation instanceof eZTagsKeyword && $translation->attribute( 'language_id' ) != $tag->attribute( 'main_language_id' ) )
+			{
+				$translation->remove();
+			}
+		}
+		$db->commit();
+	}
 }
 else if ( $http->hasPostVariable( 'UpdateMainLanguageButton' ) )
 {
+	if ( $http->hasPostVariable( 'MainLanguageID' ) )
+	{
+		$db->begin();
+		$tag->updateMainTranslation( $http->postVariable( 'MainLanguageID' ), true );
+		$tag->updateModified();
+		$db->commit();
+	}
 }
 else if ( $http->hasPostVariable( 'UpdateAlwaysAvailableButton' ) )
 {
+	$db->begin();
+	$alwaysAvailable = $http->hasPostVariable( 'AlwaysAvailable' );
+	$tag->setAlwaysAvailable( $alwaysAvailable, true );
+	$db->commit();
 }
 
 return $Module->redirectToView( 'id', array( $tag->attribute( 'id' ) ) );
