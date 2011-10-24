fe7eb92b3a176e0c39147691810f7c665ddc15a6
Edi Modric <edi.modric@gmail.com>
2011-10-24 18:27:07 +0000

    Update schema with column defaults

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -27,10 +27,10 @@ CREATE TABLE `eztags_attribute_link` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `eztags_keyword` (
-  `keyword_id` int(11) NOT NULL,
-  `language_id` int(11) NOT NULL,
+  `keyword_id` int(11) NOT NULL default '0',
+  `language_id` int(11) NOT NULL default '0',
   `keyword` varchar(255) NOT NULL default '',
   `locale` varchar(255) NOT NULL default '',
-  `status` int(11) NOT NULL,
+  `status` int(11) NOT NULL default '0',
   PRIMARY KEY ( `keyword_id`, `locale` )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
