e3d1442782bc893af9dddb5c2c9e598c611490a3
Edi Modric <edi.modric@gmail.com>
2011-11-10 16:42:54 +0000

    Fix conflict in js now that all scripts are in design.ini

--- design/standard/javascript/eztagsselectparent.js
+++ design/standard/javascript/eztagsselectparent.js
@@ -4,26 +4,28 @@ jQuery(document).ready(function($) {
 		parent_selector_tree = $('.parent-selector-tree'),
 		parent_id, parent_keyword;
 
-	parent_selector_buttons.click(function()
-	{
-		parent_id = $('#' + $(this).attr('id').replace('eztags-parent-selector-button-', 'eztags_parent_id_'));
-		parent_keyword = $('#' + $(this).attr('id').replace('eztags-parent-selector-button-', 'eztags_parent_keyword_'));
-		parent_selector_tree.jqmShow(); return false;
-	});
-	parent_selector_tree.jqm({modal:true, overlay:60, overlayClass:'whiteOverlay'}).jqDrag('.jqDrag');
+	if ( parent_selector_buttons.length > 0 && parent_selector_tree.length > 0 ) {
+		parent_selector_buttons.click(function()
+		{
+			parent_id = $('#' + $(this).attr('id').replace('eztags-parent-selector-button-', 'eztags_parent_id_'));
+			parent_keyword = $('#' + $(this).attr('id').replace('eztags-parent-selector-button-', 'eztags_parent_keyword_'));
+			parent_selector_tree.jqmShow(); return false;
+		});
+		parent_selector_tree.jqm({modal:true, overlay:60, overlayClass:'whiteOverlay'}).jqDrag('.jqDrag');
 
-	function getParentTagHierarchy(tag, i) {
-		if (tag.attr('rel') == 0) if (i == 0) return '(no parent)'; else return '';
-		var parent = getParentTagHierarchy(tag.parents('div:first').prev('a'), ++i);
-		return (parent ? parent + ' / ' : '') + tag.parent().find('span').html();
+		function getParentTagHierarchy(tag, i) {
+			if (tag.attr('rel') == 0) if (i == 0) return '(no parent)'; else return '';
+			var parent = getParentTagHierarchy(tag.parents('div:first').prev('a'), ++i);
+			return (parent ? parent + ' / ' : '') + tag.parent().find('span').html();
+		}
+
+		$('.contentstructure a:not([class^="openclose"])').live('click', function(e) {
+			if ($(this).parents('li.disabled').length) return false;
+			var tag = $(this);
+			parent_keyword.html(getParentTagHierarchy(tag, 0));
+			parent_id.val(tag.attr('rel'));
+			parent_selector_tree.jqmHide();
+			return false;
+		});
 	}
-
-	$('.contentstructure a:not([class^="openclose"])').live('click', function(e) {
-		if ($(this).parents('li.disabled').length) return false;
-		var tag = $(this);
-		parent_keyword.html(getParentTagHierarchy(tag, 0));
-		parent_id.val(tag.attr('rel'));
-		parent_selector_tree.jqmHide();
-		return false;
-	});
 });
