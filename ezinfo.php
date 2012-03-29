a5299f549de0ac354bf55284421ee86df3b1ccc5
Edi Modric <edi.modric@gmail.com>
2012-03-29 13:14:53 +0000

    Update ezinfo.php & extension.xml

--- ezinfo.php
+++ ezinfo.php
@@ -1,35 +1,13 @@
 <?php
-//
-// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
-// SOFTWARE NAME: eZ Tags
-// SOFTWARE RELEASE: 1.2.2
-// COPYRIGHT NOTICE: Copyright (C) 2010-2012 Netgen d.o.o., 1999-2012 eZ Systems AS
-// SOFTWARE LICENSE: GNU General Public License v2.0
-// NOTICE: >
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of version 2.0  of the GNU General
-//   Public License as published by the Free Software Foundation.
-//
-//   This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of version 2.0 of the GNU General
-//   Public License along with this program; if not, write to the Free
-//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//   MA 02110-1301, USA.
-// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
-//
 
 class eztagsInfo
 {
     static function info()
     {
         return array( 'Name'      => '<a href="http://projects.ez.no/eztags">eZ Tags</a> extension',
-                      'Version'   => '1.2.2',
+                      'Version'   => '//autogentag//',
                       'Copyright' => 'Copyright (C) 2010-2012 Netgen d.o.o., 1999-2012 eZ Systems AS',
-                      'License'   => 'GNU General Public License v2.0' );
+                      'License'   => '//EZP_LICENSE//' );
     }
 }
 
