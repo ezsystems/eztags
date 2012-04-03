818ca91069830e5621dd9a07701ae81e8adbcc20
Edi Modric <edi.modric@gmail.com>
2012-04-03 12:57:57 +0000

    Remove unneeded config settings from extendedattributefilter.ini

--- settings/extendedattributefilter.ini.append.php
+++ settings/extendedattributefilter.ini.append.php
@@ -1,8 +1,6 @@
 <?php /* #?ini charset="utf-8"?
 
 [TagsAttributeFilter]
-ExtensionName=eztags
 ClassName=eZTagsAttributeFilter
 MethodName=createSqlParts
-FileName=classes/eztagsattributefilter.php
 */ ?>
