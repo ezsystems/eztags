7358bf5c591cf5278dd90cf7af06e21d9df69609
Edi Modrić <edi@netgen.hr>
2014-02-06 09:14:20 +0000

    We are in 2014

--- ezinfo.php
+++ ezinfo.php
@@ -6,7 +6,7 @@ class eztagsInfo
     {
         return array( 'Name'      => '<a href="http://projects.ez.no/eztags">eZ Tags LS</a> extension',
                       'Version'   => '//autogentag//',
-                      'Copyright' => 'Copyright (C) 2010-2013 Netgen d.o.o., 1999-2013 eZ Systems AS',
+                      'Copyright' => 'Copyright (C) 2010-2014 Netgen d.o.o., 1999-2014 eZ Systems AS',
                       'License'   => '//EZP_LICENSE//' );
     }
 }
