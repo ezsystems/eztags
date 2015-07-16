a32443accb57984ab99300dbba1b4bf3362fe186
wizhippo <wizhippo@gmail.com>
2015-07-16 09:16:22 -0400

    add idx_ prefix to indexes in sql schema

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -8,9 +8,9 @@ CREATE TABLE `eztags` (
   `modified` int(11) NOT NULL default '0',
   `remote_id` varchar(100) NOT NULL default '',
   PRIMARY KEY ( `id` ),
-  UNIQUE KEY `eztags_remote_id` (`remote_id`),
-  KEY `eztags_keyword` ( `keyword` ),
-  KEY `eztags_keyword_id` ( `keyword`, `id` )
+  KEY `idx_eztags_keyword` ( `keyword` ),
+  KEY `idx_eztags_keyword_id` ( `keyword`, `id` ),
+  UNIQUE KEY `idx_eztags_remote_id` ( `remote_id` )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `eztags_attribute_link` (
@@ -21,8 +21,8 @@ CREATE TABLE `eztags_attribute_link` (
   `object_id` int(11) NOT NULL default '0',
   `priority` int(11) NOT NULL default '0',
   PRIMARY KEY ( `id` ),
-  KEY `eztags_attr_link_keyword_id` ( `keyword_id` ),
-  KEY `eztags_attr_link_kid_oaid_oav` ( `keyword_id`, `objectattribute_id`, `objectattribute_version` ),
-  KEY `eztags_attr_link_kid_oid` ( `keyword_id`, `object_id` ),
-  KEY `eztags_attr_link_oaid_oav` ( `objectattribute_id`, `objectattribute_version` )
+  KEY `idx_eztags_attr_link_keyword_id` ( `keyword_id` ),
+  KEY `idx_eztags_attr_link_kid_oaid_oav` ( `keyword_id`, `objectattribute_id`, `objectattribute_version` ),
+  KEY `idx_eztags_attr_link_kid_oid` ( `keyword_id`, `object_id` ),
+  KEY `idx_eztags_attr_link_oaid_oav` ( `objectattribute_id`, `objectattribute_version` )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- sql/postgresql/schema.sql
+++ sql/postgresql/schema.sql
@@ -9,12 +9,12 @@ CREATE TABLE eztags (
    modified integer NOT NULL default 0,
    remote_id varchar(100) NOT NULL default '',
    PRIMARY KEY (id),
-   CONSTRAINT remote_id UNIQUE  (remote_id)
+   CONSTRAINT idx_eztags_remote_id UNIQUE  (remote_id)
 );
-CREATE INDEX eztags_keyword ON eztags (
+CREATE INDEX idx_eztags_keyword ON eztags (
    keyword
 );
-CREATE INDEX eztags_keyword_id ON eztags (
+CREATE INDEX idx_eztags_keyword_id ON eztags (
    keyword,
    id
 );
@@ -28,19 +28,19 @@ CREATE TABLE eztags_attribute_link (
    priority integer not null default 0,
    PRIMARY KEY (id)
 );
-CREATE INDEX eztags_attr_link_keyword_id ON eztags_attribute_link (
+CREATE INDEX idx_eztags_attr_link_keyword_id ON eztags_attribute_link (
    keyword_id
 );
-CREATE INDEX eztags_attr_link_kid_oaid_oav ON eztags_attribute_link (
+CREATE INDEX idx_eztags_attr_link_kid_oaid_oav ON eztags_attribute_link (
    keyword_id,
    objectattribute_id,
    objectattribute_version
 );
-CREATE INDEX eztags_attr_link_kid_oid ON eztags_attribute_link (
+CREATE INDEX idx_eztags_attr_link_kid_oid ON eztags_attribute_link (
    keyword_id,
    object_id
 );
-CREATE INDEX eztags_attr_link_oaid_oav ON eztags_attribute_link (
+CREATE INDEX idx_eztags_attr_link_oaid_oav ON eztags_attribute_link (
    objectattribute_id,
    objectattribute_version
 );
